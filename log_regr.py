683d2b72a32fa7cb8ce96f6ecec1b38fda8fd4dc
DefinatelyNotBach <bachnv06@gmail.com>
2025-05-07 18:41:19 +0700

    Update focal loss for lab3

--- log_regr.py
+++ log_regr.py
@@ -59,6 +59,63 @@ def gradient_descent(X1, X2, y, lr, iter):
 
     return loss, iterations
 
+def focal_loss(X1, X2, y, w1, w2, w0):
+    m = len(y)
+    loss = 0
+    gamma = 2 # [0, 5]
+    alpha = 0.5
+    for i in range(m):
+        z = w0 + w1 * X1[i] + w2 * X2[i]
+        y_hat = sigmoid(z)
+        t1 = y[i] * ((1 - y_hat) ** gamma) * log(y_hat)
+        t2 = (1 - y[i]) * (y_hat ** gamma) * log(1 - y_hat)
+        loss = loss + ( -alpha * (t1 + t2) )
+    return loss / m
+
+def focal_gradients(X1, X2, y, w1, w2, w0):
+    m = len(y)
+    dw1 = 0.0
+    dw2 = 0.0
+    dw0 = 0.0
+    alpha = 2
+    gamma = 0.5
+    
+    for i in range(m):
+        z = w0 + w1 * X1[i] + w2 * X2[i]
+        y_hat = sigmoid(z)
+
+        t1 = (1 - y[i]) * (y_hat ** gamma) * (1 - y_hat) * (gamma * log(1 - y_hat) + y_hat / (1 - y_hat))
+        t2 = y[i] * ((1 - y_hat) ** gamma) * y_hat * (gamma * log(y_hat) + (1 - y_hat) / y_hat)
+        dz = alpha * (t1 - t2)
+
+        # Chain rule
+        dw1 += dz * X1[i]  
+        dw2 += dz * X2[i] 
+        dw0 += dz  
+
+    return dw1/m , dw2/m , dw0/m
+
+def focal_gradient_descent(X1, X2, y, lr, iter):
+    w1 = 0.0
+    w2 = 0.0
+    w0 = 0.0
+    loss = []
+    iterations = [i for i in range(iter)]
+    
+    for i in range(iter):
+
+        loss_val = focal_loss(X1, X2, y, w1, w2, w0)
+        loss.append(loss_val)
+        # Compute gradients
+        dw1, dw2, dw0 = focal_gradients(X1, X2, y, w1, w2, w0)
+        # Update weights
+        w1 = w1 - lr * dw1
+        w2 = w2 - lr * dw2
+        w0 = w0 - lr * dw0
+
+        print(f"Iteration {i}: Loss={loss_val}, w0={w0}, w1={w1}, w2={w2}")
+
+    return loss, iterations
 
 X1 = [] #Experience
 X2 = [] #Salary
@@ -78,7 +135,7 @@ with open('loan2.csv', 'r') as file:
 
 lr = 0.1
 iter = 100
-loss, iter = gradient_descent(X1, X2, y, lr, iter)
+loss, iter = focal_gradient_descent(X1, X2, y, lr, iter)
 
 plt.figure(figsize=(10, 8))
 plt.plot(iter, loss, label='Loss', color='blue')
